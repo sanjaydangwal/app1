ed6914de8ece68ae375a8b2b1a317ec756aa6b46
sanjay dangwal <sanjay.dangwal199@gmail.com>
2024-01-26 23:29:34 +0530

    Implement complete feature.

--- web.py
+++ web.py
@@ -14,9 +14,14 @@ st.title("My Todo App.")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="Enter a todo", label_visibility='hidden', placeholder="Add new todo.....", key="new_todo",
               on_change=add_todo)
-st.session_state
+
